e551f2b9b951aa5d3cf04d01892ac9e93cca3166
Ivan Korneychuk <ikorneyc@uwaterloo.ca>
2024-04-17 18:06:58 -0400

    removed components

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,8 +1,6 @@
 "use client";
 import React from "react";
 import { SignInButton, SignedOut, SignedIn, UserButton } from "@clerk/nextjs";
-import { UploadButton } from "~/utils/uploadthing";
-import { useRouter } from "next/navigation";
 import { SimpleUploadButton } from "~/components/UploadButton";
 
 const TopNav = () => {
